15b100433904f8c582c72f868d22485c29f26505
Brecherbernd <68643517+Brecherbernd@users.noreply.github.com>
2020-07-23 17:37:38 +0200

    Better Haunt / Affliction - No more double casting

--- Rotations/Warlock/Brecherbernd_TheSuffering.lua
+++ Rotations/Warlock/Brecherbernd_TheSuffering.lua
@@ -1,8 +1,9 @@
+-- Big credits to Bulletin and Nemo for helping me develop my first Script, many more will come!
 local queue = {
 	"Dark Intent",
 	"Agony",
-	"Corruption",
 	"Unstable Affliction",
+	"Corruption",
 	"Haunt",
 	"Drain Soul",
 	"Malefic Grasp"
@@ -15,8 +16,9 @@ local UnstableAffliction = GetSpellInfo(30108)
 local Haunt = GetSpellInfo(48181)
 local MaleficGrasp = GetSpellInfo(103103)
 local Drainsoul = GetSpellInfo(1120)
+local lastcast = 0
 
-local UnitAffectingCombat, UnitExists, UnitCastingInfo, UnitChannelInfo, UnitIsDeadOrGhost, UnitHealth, UnitHealthMax = UnitAffectingCombat, UnitExists, UnitCastingInfo, UnitChannelInfo, UnitIsDeadOrGhost, UnitHealth, UnitHealthMax;
+local UnitAffectingCombat, UnitExists, UnitCastingInfo, UnitChannelInfo, UnitIsDeadOrGhost, UnitHealth, UnitHealthMax = UnitAffectingCombat, UnitExists, UnitCastingInfo, UnitChannelInfo, UnitIsDeadOrGhost, UnitHealth, UnitHealthMax, UnitPower;
 
 local abilities = {
 
@@ -37,6 +39,17 @@ local abilities = {
 		return true
 		end
 	end,
+
+["Unstable Affliction"] = function()
+		if ni.spell.available(UnstableAffliction)
+			and ni.unit.debuffremaining("target", "131736", "player") <= 8
+			and ni.spell.valid("target", "Haunt")
+			and GetTime() - lastcast > 4
+			then lastcast = GetTime() 
+			ni.spell.cast(UnstableAffliction, "target")
+			return true
+		end
+	end,	
 	
 ["Corruption"] = function()
 		if ni.spell.available(Corruption)
@@ -45,20 +58,19 @@ local abilities = {
 		return true
 		end
 	end,
-
-["Unstable Affliction"] = function()
-		if ni.spell.available(UnstableAffliction)
-		and ni.unit.debuffremaining("target", "131736", "player") <= 8
-		then ni.spell.cast(UnstableAffliction, "target")
-		return true
-		end
-	end,	
-
+-- Life Tap needs to be implemented into haunt
 ["Haunt"] = function()
 		if ni.spell.available(Haunt)
-		and ni.unit.debuff("target", "Corruption", "Agony", "131736", "player")
-		and ni.unit.buff("player", "117198", "player") >= 4 --Soul Shards müssen anders gecodet werden
-					then ni.spell.cast(Haunt, "target")
+		and ni.spell.valid("target", "Haunt")
+		and GetTime() - lastcast > 4
+		and UnitPower('player', 7) >= 4
+		or ni.spell.available(Haunt)
+		and ni.spell.valid("target", "Haunt")
+		and GetTime() - lastcast > 4
+		and ni.unit.buff("player", "113860")
+		and not ni.unit.debuff("target", "48181", "player")
+		then lastcast = GetTime() 
+			ni.spell.cast(Haunt, "target")
 			return true
 		end
 	end,
@@ -73,15 +85,12 @@ local abilities = {
 
 ["Malefic Grasp"] = function()
 		if ni.spell.available(MaleficGrasp)
-		and ni.unit.debuff("target", "Corruption", "Agony", "131736", "player") --Break channeling for Corruption/Agony but not Unstable Affliction.... / Further looking 
 		and not ni.unit.debuff("target", "103103", "player")
 		or ni.unit.debuff("target", "103103", "player") <= 0.25 -- (103103 is Malefic Grasp Debuff) - For higher uptime
-		and ni.unit.debuff("target", "Corruption", "Agony", "131736", "player")
 			then ni.spell.cast(MaleficGrasp, "target")
 			return true
 		end
 	end,
-	
 
 };
 ni.bootstrap.rotation("Brecherbernd_TheSuffering", queue, abilities)
