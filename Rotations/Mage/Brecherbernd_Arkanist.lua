14da7ab3d6016303684fd73a76cc3381bffc981c
Brecherbernd <68643517+Brecherbernd@users.noreply.github.com>
2020-07-27 21:45:59 +0200

    Update Brecherbernd_Arkanist.lua

--- Rotations/Mage/Brecherbernd_Arkanist.lua
+++ Rotations/Mage/Brecherbernd_Arkanist.lua
@@ -1,8 +1,13 @@
 local queue = {
-  "Arcane Brilliance",
-  "Ice Floes",
-  "Rune of Power",
-  "Mirror Image",
+	"Window",
+	"Arcane Brilliance",
+	"Conjure Mana Gem",
+	"Mage / Frost Armor",
+	"Pause",
+	"Auto Target",
+  	"Ice Floes",
+  	"Rune of Power",
+  	"Mirror Image",
 	"Living Bomb",
 	"Arcane Missiles",
 	"Arcane Barrage",
@@ -17,9 +22,35 @@ local arcanebarrage = GetSpellInfo(44425)
 local arcaneblast = GetSpellInfo(30451)
 local icefloes = GetSpellInfo(108839)
 local mirrorimage = GetSpellInfo(55342)
+local magearmor = GetSpellInfo(6117)
+local frostarmor = GetSpellInfo(7302)
+local managem = GetSpellInfo(759)
 local lastcast = 0
 
 local abilities = {
+["Pause"] = function()
+		if not UnitExists("target")
+		 or (UnitExists("target")
+		 and (not UnitCanAttack("player", "target")
+		 or UnitIsDeadOrGhost("target")))
+		 or UnitChannelInfo("player")
+		 or UnitIsDeadOrGhost("player")
+		 or IsMounted()
+		 or delayrotation then
+			return true;
+		end
+	end,
+
+["Auto Target"] = function()
+		if UnitAffectingCombat("player")
+		 and ((ni.unit.exists("target")
+		 and UnitIsDeadOrGhost("target")
+		 and not UnitCanAttack("player", "target")) 
+		 or not ni.unit.exists("target")) then
+			ni.player.runtext("/targetenemy")
+		end
+	end,
+
 ["Arcane Brilliance"] = function()
 		if ni.spell.available(arcanebrilliance) then
 			local how = ni.player.buffremaining(arcanebrilliance)
@@ -29,7 +60,27 @@ local abilities = {
 			end
 		end
 	end,	
- 
+
+["Conjure Mana Gem"] = function()
+		if not ni.player.hasitem(36799)
+		 and not ni.player.ismoving()
+		 and not UnitAffectingCombat("player")
+		 and IsUsableSpell(GetSpellInfo(759))
+		 and ni.spell.available(759) then
+			ni.spell.cast(759)
+			return true
+		end
+	end,
+
+["Mage / Frost Armor"] = function()
+    if ni.spell.available(magearmor) 
+      and not ni.unit.buff("player", 7302,"player") 
+      and not ni.unit.buff("player", 6117, "player") then
+      ni.spell.cast(magearmor, "player")
+      return true;
+    end  
+  end,
+
 ["Ice Floes"] = function()
 		if ni.spell.available(icefloes)
       and ni.unit.ismoving("player") 
@@ -66,7 +117,7 @@ local abilities = {
 		end
 	end,
 	
-["Arcane Missiles"] = function() --Channelt noch nicht durch sondern castet erneut
+["Arcane Missiles"] = function()
 	local arcanecharge, _, _, arcanecharge_stacks = ni.player.debuff(114664)
 		if arcanecharge_stacks == 4
       and ni.unit.buff("player", "79683") 
@@ -93,6 +144,13 @@ local abilities = {
 		   return true
 	   end
    end,	
-
+	
+["Window"] = function()
+		if not popup_shown then
+		 ni.debug.popup("Arkanist by Brecherbernd", 
+		 "Welcome to the Arkanist, this profile is still in developement! \nWrite me in discord if you have any suggestions!\n\nSuggested Talents: 313222\n\n\nCredits to DarhangeR, Nemo, Bulletin and Scott")	
+		popup_shown = true;
+		end 
+	end,
 };
 ni.bootstrap.rotation("Brecherbernd_Arkanist", queue, abilities)
