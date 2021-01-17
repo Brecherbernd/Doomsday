edba90bd04c68f768cf436a2db58e0c1d0ebf908
Brecherbernd <68643517+Brecherbernd@users.noreply.github.com>
2021-01-17 22:32:41 +0100

    Rewritten

--- Rotations/Mage/Brecherbernd_Arkanist.lua
+++ Rotations/Mage/Brecherbernd_Arkanist.lua
@@ -1,8 +1,75 @@
+------- Rewritten for the new ni. Big Credits to Cartman, Darhanger and Scott!
+
+local ConjureManaGem = GetSpellInfo(759)
+local arcaneintellect = GetSpellInfo(1459)
+local frostarmor = GetSpellInfo(7302)
+local magearmor = GetSpellInfo(6117)
+local runeofpower = GetSpellInfo(116011)
+local livingbomb = GetSpellInfo(44457)
+local arcanemissiles = GetSpellInfo(5143)
+local arcanebarrage = GetSpellInfo(44425)
+local arcaneblast = GetSpellInfo(30451)
+local icefloes = GetSpellInfo(108839)
+local mirrorimage = GetSpellInfo(55342)
+local lastcast = 0
+
+local items = {
+	settingsfile = "Brecherbernd_Arkanist.json",
+	{ type = "title", text = "Arkanist by |c0000CED1Brecherbernd" },
+	{ type = "separator" },
+	{ type = "page", number = 1, text = "|cffFFFF00Main Settings" },
+	{ type = "separator" },
+	{ type = "entry", text = "\124T"..select(3, GetSpellInfo(1459))..":26:26\124t Arcane Intellect", tooltip = "Arcane Intellect", enabled = true, key = "arcaneintellect" },
+	{ type = "separator" },
+	{ type = "title", text = "Choose your desired Armor" },
+	{ type = "separator" },
+	{ type = "entry", text = "\124T"..select(3, GetSpellInfo(7302))..":26:26\124t Frost Armor", tooltip = "Frost Armor", enabled = true, key = "frostarmor" },
+	{ type = "entry", text = "\124T"..select(3, GetSpellInfo(6117))..":26:26\124t Mage Armor", tooltip = "Mage Armor", enabled = true, key = "magearmor" },
+	-------------------
+	{ type = "page", number = 2, text = "|cffFFFF00Utility" },
+	{ type = "separator" },
+	{ type = "entry", text = "\124T"..select(3, GetSpellInfo(759))..":26:26\124t Mana Gem", tooltip = "Conjure Mana Gem", enabled = true, key = "ConjureManaGem" },
+	{ type = "entry", text = "\124T"..GetItemIcon(36799)..":26:26\124t Use Mana Gem < MP%", tooltip = "Mana Gem", enabled = true, value = 50, key = "managem" },
+};
+ 
+ 
+local function GetSetting(name)
+    for k, v in ipairs(items) do
+        if v.type == "entry"
+         and v.key ~= nil
+         and v.key == name then
+            return v.value, v.enabled
+        end
+        if v.type == "dropdown"
+         and v.key ~= nil
+         and v.key == name then
+            for k2, v2 in pairs(v.menu) do
+                if v2.selected then
+                    return v2.value
+                end
+            end
+        end
+        if v.type == "input"
+         and v.key ~= nil
+         and v.key == name then
+            return v.value
+        end
+    end
+end;
+local function OnLoad()
+	ni.GUI.AddFrame("Brecherbernd_Arkanist", items);
+end
+local function OnUnLoad()  
+	ni.GUI.DestroyFrame("Brecherbernd_Arkanist");
+end	
+ 
 local queue = {
-	"Window",
-	"Arcane Brilliance",
+    "Arcane Brilliance",
+	"Arcane Intellect",
 	"Conjure Mana Gem",
-	"Mage / Frost Armor",
+	"Mana Gem",
+	"Frost Armor",
+	"Mage Armor",
 	"Pause",
 	"Auto Target",
   	"Ice Floes",
@@ -13,21 +80,63 @@ local queue = {
 	"Arcane Barrage",
 	"Arcane Blast"
 };
-
-local runeofpower = GetSpellInfo(116011)
-local arcanebrilliance = GetSpellInfo(1459)
-local livingbomb = GetSpellInfo(44457)
-local arcanemissiles = GetSpellInfo(5143)
-local arcanebarrage = GetSpellInfo(44425)
-local arcaneblast = GetSpellInfo(30451)
-local icefloes = GetSpellInfo(108839)
-local mirrorimage = GetSpellInfo(55342)
-local magearmor = GetSpellInfo(6117)
-local frostarmor = GetSpellInfo(7302)
-local managem = GetSpellInfo(759)
-local lastcast = 0
-
+ 
 local abilities = {
+-------------------------------------------------------
+["Arcane Intellect"] = function()
+local _, enabled = GetSetting("arcaneintellect")
+        if enabled
+			and ni.spell.available(arcaneintellect) 
+            and not ni.player.buff(arcaneintellect) then
+                ni.spell.cast(arcaneintellect)
+                return true;
+			end
+		end, 		
+------------------------------------------------------------
+["Conjure Mana Gem"] = function()
+local _, enabled = GetSetting("ConjureManaGem")
+		 if enabled
+		 and ni.spell.available(ConjureManaGem) 
+		 and not ni.player.ismoving()
+		 and not UnitAffectingCombat("player")
+		 and not ni.player.hasitem(36799) then 
+			ni.spell.cast(ConjureManaGem)
+			return true;	
+		end 
+	end,	
+--------------------------------------------------------
+["Mana Gem"] = function()
+local value, enabled = GetSetting("managem")
+		 if enabled
+		 and ni.player.power() <= value
+		 and ni.player.hasitem(36799) then 
+			ni.player.useitem(36799)
+			return true;	
+		end 
+	end,	
+	
+-------------------------------------------------------
+["Frost Armor"] = function()
+local _, enabled = GetSetting("frostarmor")
+        if enabled
+			and ni.spell.available(frostarmor) 
+			and not ni.player.buff(frostarmor) then
+				ni.spell.cast(frostarmor)
+				return true;
+		end
+	end, 
+	
+-------------------------------------------------------
+["Mage Armor"] = function()
+local _, enabled = GetSetting("magearmor")
+		if enabled
+			and ni.spell.available(magearmor) 
+			and not ni.player.buff(magearmor) then
+				ni.spell.cast(magearmor)
+				return true;
+		end
+	end, 	
+
 ["Pause"] = function()
 		if not UnitExists("target")
 		 or (UnitExists("target")
@@ -50,38 +159,7 @@ local abilities = {
 			ni.player.runtext("/targetenemy")
 		end
 	end,
-
-["Arcane Brilliance"] = function()
-		if ni.spell.available(arcanebrilliance) then
-			local how = ni.player.buffremaining(arcanebrilliance)
-			if how <= 1 then
-				ni.spell.cast(arcanebrilliance)
-				return true;
-			end
-		end
-	end,	
-
-["Conjure Mana Gem"] = function()
-		if not ni.player.hasitem(36799)
-		 and not ni.player.ismoving()
-		 and not UnitAffectingCombat("player")
-		 and IsUsableSpell(GetSpellInfo(759))
-		 and ni.spell.available(759) then
-			ni.spell.cast(759)
-			return true
-		end
-	end,
-
-["Mage / Frost Armor"] = function()
-    if ni.spell.available(magearmor) 
-      and not ni.unit.buff("player", 7302,"player") 
-      and not ni.unit.buff("player", 6117, "player") then
-      ni.spell.cast(magearmor, "player")
-      return true;
-    end  
-  end,
-
-["Ice Floes"] = function()
+	["Ice Floes"] = function()
 		if ni.spell.available(icefloes)
       and ni.unit.ismoving("player") 
       and not ni.unit.buff("player", 108839, "player") then
@@ -144,6 +222,5 @@ local abilities = {
 		   return true
 	   end
    end,	
-
 };
-ni.bootstrap.rotation("Brecherbernd_Arkanist", queue, abilities)
+ni.bootstrap.profile("Brecherbernd_Arkanist", queue, abilities, OnLoad, OnUnLoad);	
